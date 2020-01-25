a9d30536cc929173cdae4dc0db0b6b70db169979
Lyudmil Latinov <llatinov@gmail.com>
2020-01-25 12:01:25 +0100

    Add new models

---
+++ Models/src/LogEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Models
+{
+    public class LogEntry
+    {
+        public string Message { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ Models/src/SqsMessageAttributes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Amazon.SQS.Model;
+
+namespace Models
+{
+    public static class SqsMessageTypeAttribute
+    {
+        private const string AttributeName = "MessageType";
+
+        public static string GetAttributeValue(this Dictionary<string, MessageAttributeValue> attributes)
+        {
+            return attributes.SingleOrDefault(x => x.Key == AttributeName).Value?.StringValue;
+        }
+
+        public static Dictionary<string, MessageAttributeValue> CreateAttributes<T>()
+        {
+            return CreateAttributes(typeof(T).Name);
+        }
+
+        public static Dictionary<string, MessageAttributeValue> CreateAttributes(string messageType)
+        {
+            return new Dictionary<string, MessageAttributeValue>
+            {
+                {
+                    AttributeName, new MessageAttributeValue
+                    {
+                        DataType = nameof(String),
+                        StringValue = messageType
+                    }
+                }
+            };
+        }
+    }
+}
